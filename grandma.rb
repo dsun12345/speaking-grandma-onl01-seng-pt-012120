0dccf068e8b899ffb0aab000858f851ba3964c16
Daniel Sun <danielsun12345@gmail.com>
2020-01-24 01:54:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "WHAT DID YOU EAT TODAY?"
-    "NO, NOT SINCE 1938!"
-  elsif phrase == "WHAT?"
-    "NO, NOT SINCE 1938!"
-  else 
+  elsif phrase != phrase.upcase()
     "HUH?! SPEAK UP, SONNY!"
+  else
+    "NO, NOT SINCE 1938"
   end
 end
