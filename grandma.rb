83f47d51d907ea56129b0a5049758ef7dfec2546
Daniel Sun <danielsun12345@gmail.com>
2020-01-24 01:47:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == 
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
+  
   else 
     "HUH?! SPEAK UP, SONNY!"
   end
