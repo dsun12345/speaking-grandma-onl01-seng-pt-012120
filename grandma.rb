2ea9a27ade93f0e9f1b995730c79347c3f9ca75f
Daniel Sun <danielsun12345@gmail.com>
2020-01-24 01:28:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,1 @@
-speak_to_grandma = ""
-  if speak_to_grandma == speak_to_grandma.upcase
-    puts "NO, NOT SINCE 1938!"
-  elsif speak_to_grandma == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN"
-  else 
-    puts "HUH?! SPEAK UP, SONNY"
-  end
+
